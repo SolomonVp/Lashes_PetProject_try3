64afa626253c8ec4cafa6c141796498edd394361
SolomonVP <Lum7023@yandex.ru>
2022-12-06 12:41:00 +0300

    add aop

---
+++ src/main/java/ru/lashes/aspect/MyLoggingAspect.java
@@ -0,0 +1,21 @@
+package ru.lashes.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+    @Around("execution(* ru.lashes.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+        System.out.println("Begin of " + methodName);
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("End of " + methodName);
+        return targetMethodResult;
+    }
+}
